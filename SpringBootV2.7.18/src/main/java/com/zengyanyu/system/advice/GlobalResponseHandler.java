5c7a3b4aea05171956a9ae42745c6f55e3a8b848
zengyanyu <1194314874@qq.com>
2026-02-28 11:28:22 +0800

    添加通用返回处理器

---
+++ SpringBootV2.7.18/src/main/java/com/zengyanyu/system/advice/GlobalResponseHandler.java
@@ -0,0 +1,42 @@
+package com.zengyanyu.system.advice;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+/**
+ * 全局响应处理器
+ *
+ * @author zengyanyu
+ */
+@RestControllerAdvice
+public class GlobalResponseHandler implements ResponseBodyAdvice<Object> {
+
+    /**
+     * @param returnType
+     * @param converterType
+     * @return
+     */
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return true;
+    }
+
+    /**
+     * @param body
+     * @param returnType
+     * @param selectedContentType
+     * @param selectedConverterType
+     * @param request
+     * @param response
+     * @return
+     */
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        return body;
+    }
+}
