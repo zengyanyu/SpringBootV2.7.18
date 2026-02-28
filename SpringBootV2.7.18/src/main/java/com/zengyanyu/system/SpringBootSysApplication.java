3f2efe0ebbea1c981c3e0ea67fb38fc8aecde413
zengyanyu <1194314874@qq.com>
2026-02-28 15:22:51 +0800

    优化

--- SpringBootV2.7.18/src/main/java/com/zengyanyu/system/SpringBootSysApplication.java
+++ SpringBootV2.7.18/src/main/java/com/zengyanyu/system/SpringBootSysApplication.java
@@ -1,18 +1,48 @@
 package com.zengyanyu.system;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.net.InetAddress;
 
 /**
  * @author zengyanyu
  */
+@Slf4j
 @EnableScheduling
 @SpringBootApplication
 public class SpringBootSysApplication {
 
     public static void main(String[] args) {
-        //
-        SpringApplication.run(SpringBootSysApplication.class, args);
+        ConfigurableApplicationContext ioc = SpringApplication.run(SpringBootSysApplication.class, args);
+
+        ConfigurableEnvironment env = ioc.getEnvironment();
+        logApplicationStartup(env);
+    }
+
+    private static void logApplicationStartup(ConfigurableEnvironment env) {
+        String protocol = "http";
+        String serverPort = env.getProperty("server.port");
+        String contextPath = env.getProperty("server.servlet.context-path");
+        if (StringUtils.isBlank(contextPath)) {
+            contextPath = "/doc.html";
+        } else {
+            contextPath = contextPath + "/doc.html";
+        }
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (Exception e) {
+        }
+        log.info("应用程序“{}”正在运行中......", env.getProperty("spring.application.name"));
+        log.info("接口文档访问 URL:");
+        log.info("本地: \t\t{}://localhost:{}{}", protocol, serverPort, contextPath);
+        log.info("外部: \t{}://{}:{}{}", protocol, hostAddress, serverPort, contextPath);
+        log.info("配置文件: \t{}", env.getActiveProfiles());
     }
 }
