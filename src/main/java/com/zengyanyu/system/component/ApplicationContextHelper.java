9088f4c540cb414888bd9215a0f2386e927ec676
zengyanyu <1194314874@qq.com>
2026-03-18 16:30:09 +0800

    优化

--- src/main/java/com/zengyanyu/system/component/ApplicationContextHelper.java
+++ src/main/java/com/zengyanyu/system/component/ApplicationContextHelper.java
@@ -8,13 +8,12 @@ package com.zengyanyu.system.component;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
 /**
- * spring 容器工具
+ * Spring 上下文工具类（非 Spring 管理的类中获取 Bean/配置）
  *
  * @author zengyanyu
  */
@@ -24,27 +23,42 @@ public class ApplicationContextHelper implements ApplicationContextAware {
     private static ApplicationContext applicationContext;
 
     @Override
-    public void setApplicationContext(@NonNull ApplicationContext applicationContext) throws BeansException {
-        synchronized (ApplicationContextHelper.class) {
-            ApplicationContextHelper.applicationContext = applicationContext;
-        }
-    }
-
-    public static <T> T getBean(Class<T> requiredType) {
-        return applicationContext != null ? applicationContext.getBean(requiredType) : null;
-    }
-
-    public static Object getBean(String name) {
-        return applicationContext != null ? applicationContext.getBean(name) : null;
-    }
-
-
-    public static <T> Map<String, T> getBeanOfType(Class<T> clazz) {
-        return applicationContext != null ? applicationContext.getBeansOfType(clazz) : null;
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        ApplicationContextHelper.applicationContext = applicationContext;
     }
 
     /**
-     * 手动获取配置值
+     * 根据类型获取 Bean
+     *
+     * @param requiredType
+     * @param <T>
+     * @return
+     */
+    public static <T> T getBean(Class<T> requiredType) {
+        return applicationContext.getBean(requiredType);
+    }
+
+    /**
+     * 根据名称获取 Bean
+     *
+     * @param name
+     * @return
+     */
+    public static Object getBean(String name) {
+        return applicationContext.getBean(name);
+    }
+
+    /**
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T> Map<String, T> getBeanOfType(Class<T> clazz) {
+        return applicationContext.getBeansOfType(clazz);
+    }
+
+    /**
+     * 获取环境变量
      *
      * @param key
      * @return
